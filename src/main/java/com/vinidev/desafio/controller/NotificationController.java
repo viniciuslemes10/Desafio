97fa84a7912606a30c60efcdbf18b9c9a2bc8388
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-05 15:57:20 -0300

    feat: creating notification search

--- src/main/java/com/vinidev/desafio/controller/NotificationController.java
+++ src/main/java/com/vinidev/desafio/controller/NotificationController.java
@@ -1,17 +1,15 @@
 package com.vinidev.desafio.controller;
 
 import com.vinidev.desafio.controller.dto.ScheduleNotificationDTO;
+import com.vinidev.desafio.entity.Notification;
 import com.vinidev.desafio.service.NotificationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
 public class NotificationController {
-    private NotificationService notificationService;
+    private final NotificationService notificationService;
 
     public NotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
@@ -22,5 +20,14 @@ public class NotificationController {
         notificationService.scheduleNotification(dto);
 
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Notification> getNotifications(@PathVariable("id") Long id) {
+        var notification = notificationService.getNotifications(id);
+
+        if(notification.isEmpty()) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(notification.get());
     }
 }
--- src/main/java/com/vinidev/desafio/service/NotificationService.java
+++ src/main/java/com/vinidev/desafio/service/NotificationService.java
@@ -1,8 +1,11 @@
 package com.vinidev.desafio.service;
 
 import com.vinidev.desafio.controller.dto.ScheduleNotificationDTO;
+import com.vinidev.desafio.entity.Notification;
 import com.vinidev.desafio.repositories.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class NotificationService {
@@ -15,5 +18,9 @@ public class NotificationService {
 
     public void scheduleNotification(ScheduleNotificationDTO dto) {
         notificationRepository.save(dto.toNotification());
+    }
+
+    public Optional<Notification> getNotifications(Long id) {
+        return notificationRepository.findById(id);
     }
 }
