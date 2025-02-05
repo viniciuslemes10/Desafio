8612a2894d38f25b66d682da609e0291dcb603af
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-05 14:34:13 -0300

    feat: mapping entities

---
+++ src/main/java/com/vinidev/desafio/entity/Channel.java
@@ -0,0 +1,33 @@
+package com.vinidev.desafio.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_channel")
+public class Channel {
+    @Id
+    private Long id;
+
+    private String description;
+
+    public Channel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
