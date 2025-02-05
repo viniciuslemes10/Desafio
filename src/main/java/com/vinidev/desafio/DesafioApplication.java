5af6bc4a1d9857ea66ff7a34f2f6683916da82e1
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-05 16:15:35 -0300

    feat: creating checking routine via Spring Scheduler

--- src/main/java/com/vinidev/desafio/DesafioApplication.java
+++ src/main/java/com/vinidev/desafio/DesafioApplication.java
@@ -2,8 +2,10 @@ package com.vinidev.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DesafioApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/vinidev/desafio/schedule/DesafioTaskScheduler.java
@@ -0,0 +1,21 @@
+package com.vinidev.desafio.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class DesafioTaskScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(DesafioTaskScheduler.class);
+
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    public void runTasks() {
+        var dateTime = LocalDateTime.now();
+        logger.info("Running at {},", dateTime);
+    }
+}
